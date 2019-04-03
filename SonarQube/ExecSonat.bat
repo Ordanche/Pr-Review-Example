214a77e1c229f03ab94dee9d861eab29f8571e77
Ordanche <42156892+Ordanche@users.noreply.github.com>
2019-04-03 22:24:53 +0200

    Delete ExecSonat.bat

--- SonarQube/ExecSonat.bat
+++
@@ -1,3 +0,0 @@
-"./bin/SonarScanner.MSBuild.exe" begin /k:"Ordanche_Pr-Review-Example" /d:sonar.organization="ordanche-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="5a60c1f920227b60940537fb21c530e1542c7b42"
-"MsBuild.exe" ../Demo.sln /t:Rebuild
-"./bin/SonarScanner.MSBuild.exe" end /d:sonar.login="5a60c1f920227b60940537fb21c530e1542c7b42"
